a431a162c3f2d1685e4634a7107a06b8c0e1551a
Joyce Echessa <jokhessa@yahoo.com>
2018-02-24 14:14:44 +0300

    Refactor to separate concerns

--- ANGULAR/NG_BOOK_2/angular-reddit/src/app/article/article.component.ts
+++ ANGULAR/NG_BOOK_2/angular-reddit/src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -7,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
 
-  constructor() { }
+  @HostBinding('attr.class') cssClass = 'row';
+  article: Article;
+
+  constructor() {
+    this.article = new Article(
+      'Angular 2',
+      'http://angular.io',
+      10);
+  }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
 
   ngOnInit() {
   }

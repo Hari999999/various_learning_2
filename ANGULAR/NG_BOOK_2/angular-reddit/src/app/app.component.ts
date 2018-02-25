101a04a5dc3df6535b14167b0d718dfc5609af88
Joyce Echessa <jokhessa@yahoo.com>
2018-02-25 06:27:06 +0300

    Enable addition of Article

--- ANGULAR/NG_BOOK_2/angular-reddit/src/app/app.component.ts
+++ ANGULAR/NG_BOOK_2/angular-reddit/src/app/app.component.ts
@@ -14,12 +14,15 @@ export class AppComponent {
     this.articles = [
       new Article('Angular 2', 'http://angular.io', 3),
       new Article('Fullstack', 'http://fullstack.io', 2),
-      new Article('Angular Homepage', 'http://angular.io', 1),
+      new Article('Angular Homepage', 'http://angular.io', 1)
     ];
   }
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
   }
 

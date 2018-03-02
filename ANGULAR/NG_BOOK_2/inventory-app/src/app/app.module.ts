b5a760fe191db0fb3d876ec8a2b4d6bb554568f7
Joyce Echessa <jokhessa@yahoo.com>
2018-03-02 08:55:16 +0300

    Add PriceDisplayComponent

--- ANGULAR/NG_BOOK_2/inventory-app/src/app/app.module.ts
+++ ANGULAR/NG_BOOK_2/inventory-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PriceDisplayComponent
   ],
   imports: [
     BrowserModule

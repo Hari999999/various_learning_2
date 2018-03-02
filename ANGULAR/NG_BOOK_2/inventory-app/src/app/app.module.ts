b201b16afc34b015c88cd34b001e254283870e5a
Joyce Echessa <jokhessa@yahoo.com>
2018-03-02 08:57:01 +0300

    Add ProductImageComponent

--- ANGULAR/NG_BOOK_2/inventory-app/src/app/app.module.ts
+++ ANGULAR/NG_BOOK_2/inventory-app/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PriceDisplayComponent } from './price-display/price-display.component';
 import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { ProductImageComponent } from './product-image/product-image.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PriceDisplayComponent,
-    ProductDepartmentComponent
+    ProductDepartmentComponent,
+    ProductImageComponent
   ],
   imports: [
     BrowserModule

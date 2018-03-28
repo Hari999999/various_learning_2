eee876a2115b9c55dd163967c7cbbb04c47691c7
Joyce Echessa <jokhessa@yahoo.com>
2018-03-28 18:51:38 +0300

    Inflate CrimeFragment view

--- ANDROID/CriminalIntent/app/src/main/java/com/echessa/criminalintent/CrimeActivity.java
+++ ANDROID/CriminalIntent/app/src/main/java/com/echessa/criminalintent/CrimeActivity.java
@@ -1,5 +1,7 @@
 package com.echessa.criminalintent;
 
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
--- ANDROID/CriminalIntent/app/src/main/java/com/echessa/criminalintent/CrimeFragment.java
+++ ANDROID/CriminalIntent/app/src/main/java/com/echessa/criminalintent/CrimeFragment.java
@@ -1,10 +1,30 @@
 package com.echessa.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by echessa on 3/28/18.
  */
 
 public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        return v;
+    }
 }

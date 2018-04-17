1b15b7fff237df8f5476bc5c37ef691bdca02191
Joyce Echessa <jokhessa@yahoo.com>
2018-04-17 10:14:12 +0300

    Show beer choices on selecting the type

--- ANDROID/HF/BeerAdviser/app/src/main/java/com/echessa/beeradviser/FindBeerActivity.java
+++ ANDROID/HF/BeerAdviser/app/src/main/java/com/echessa/beeradviser/FindBeerActivity.java
@@ -2,12 +2,34 @@ package com.echessa.beeradviser;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class FindBeerActivity extends Activity {
+
+    private TextView brands;
+    private Spinner color;
+    private BeerExpert expert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+
+        brands = findViewById(R.id.brands);
+        color = findViewById(R.id.color);
+    }
+
+    public void onClickFindBeer(View view) {
+        String beerType = String.valueOf(color.getSelectedItem());
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+        brands.setText(brandsFormatted);
     }
 }

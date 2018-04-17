0eded84629568b466df7367cbc36151cdff47e2a
Joyce Echessa <jokhessa@yahoo.com>
2018-04-17 17:03:34 +0300

    Pass message to ReceiveMessageActivity

--- ANDROID/HF/MyMessenger/app/src/main/java/com/echessa/mymessenger/CreateMessageActivity.java
+++ ANDROID/HF/MyMessenger/app/src/main/java/com/echessa/mymessenger/CreateMessageActivity.java
@@ -1,17 +1,27 @@
 package com.echessa.mymessenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends Activity {
+
+    private EditText messageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+
+        messageView = findViewById(R.id.message);
     }
 
     public void onSendMessage(View view) {
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
     }
 }
--- ANDROID/HF/MyMessenger/app/src/main/java/com/echessa/mymessenger/ReceiveMessageActivity.java
+++ ANDROID/HF/MyMessenger/app/src/main/java/com/echessa/mymessenger/ReceiveMessageActivity.java
@@ -1,13 +1,23 @@
 package com.echessa.mymessenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceiveMessageActivity extends Activity {
+
+    public static final String EXTRA_MESSAGE = "message";
+    private TextView messageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        messageView = findViewById(R.id.message);
+        messageView.setText(messageText);
     }
 }

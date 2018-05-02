6c304c4374c23eace3a247ea1c58db6cabd1c862
Joyce Echessa <jokhessa@yahoo.com>
2018-05-02 13:48:28 +0300

    Create Starbuzz project

---
+++ ANDROID/HF/Starbuzz/app/src/main/java/com/echessa/starbuzz/TopLevelActivity.java
@@ -0,0 +1,13 @@
+package com.echessa.starbuzz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TopLevelActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_level);
+    }
+}

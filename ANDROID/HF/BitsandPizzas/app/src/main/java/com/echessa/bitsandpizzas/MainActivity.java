1b3423560629438a395e7cf52b63399db0d11382
Joyce Echessa <jokhessa@yahoo.com>
2018-05-03 23:48:28 +0300

    Menu

--- ANDROID/HF/BitsandPizzas/app/src/main/java/com/echessa/bitsandpizzas/MainActivity.java
+++ ANDROID/HF/BitsandPizzas/app/src/main/java/com/echessa/bitsandpizzas/MainActivity.java
@@ -1,8 +1,11 @@
 package com.echessa.bitsandpizzas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                //Code to run when the Create Order item is clicked
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }

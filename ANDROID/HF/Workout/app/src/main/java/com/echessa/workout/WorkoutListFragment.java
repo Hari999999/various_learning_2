ba9dfa8c6b46a9229b99d4ec52493e1405420a3f
Joyce Echessa <jokhessa@yahoo.com>
2018-05-15 11:59:04 +0300

    Create WorkoutListFragment

---
+++ ANDROID/HF/Workout/app/src/main/java/com/echessa/workout/WorkoutListFragment.java
@@ -0,0 +1,27 @@
+package com.echessa.workout;
+
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link ListFragment} subclass.
+ */
+public class WorkoutListFragment extends ListFragment {
+
+
+    public WorkoutListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}

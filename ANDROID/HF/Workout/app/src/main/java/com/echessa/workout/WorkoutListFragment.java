d87d032d56e2450b10d0579f9f13a8dc66d165dc
Joyce Echessa <jokhessa@yahoo.com>
2018-05-15 14:49:20 +0300

    Remove placeholder code

--- ANDROID/HF/Workout/app/src/main/java/com/echessa/workout/WorkoutListFragment.java
+++ ANDROID/HF/Workout/app/src/main/java/com/echessa/workout/WorkoutListFragment.java
@@ -1,16 +1,25 @@
 package com.echessa.workout;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * A simple {@link ListFragment} subclass.
  */
 public class WorkoutListFragment extends ListFragment {
+
+    static interface Listener {
+        void itemClicked(long id);
+    };
+
+    private Listener listener;
 
 
     public WorkoutListFragment() {
@@ -21,7 +30,30 @@ public class WorkoutListFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+//        String[] names = new String[Workout.workouts.length];
+//        for (int i = 0; i < names.length; i++) {
+//            names[i] = Workout.workouts[i].getName();
+//        }
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(), android.R.layout.simple_list_item_1, names);
+//        setListAdapter(adapter);
+
+        ArrayAdapter<Workout> adapter = new ArrayAdapter<>(inflater.getContext(), android.R.layout.simple_list_item_1, Workout.workouts);
+        setListAdapter(adapter);
+
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.listener = (Listener)context;
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        if (listener != null) {
+            listener.itemClicked(id);
+        }
+    }
 }

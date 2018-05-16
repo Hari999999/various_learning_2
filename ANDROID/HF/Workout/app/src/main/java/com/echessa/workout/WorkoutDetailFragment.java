014ead89e5347a81f8f4f89493c855e2b1f7ce5a
Joyce Echessa <jokhessa@yahoo.com>
2018-05-16 13:35:03 +0300

    Add tablet support

--- ANDROID/HF/Workout/app/src/main/java/com/echessa/workout/WorkoutDetailFragment.java
+++ ANDROID/HF/Workout/app/src/main/java/com/echessa/workout/WorkoutDetailFragment.java
@@ -2,6 +2,8 @@ package com.echessa.workout;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +21,13 @@ public class WorkoutDetailFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            workoutId = savedInstanceState.getLong("workoutId");
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,6 +47,11 @@ public class WorkoutDetailFragment extends Fragment {
             TextView description = view.findViewById(R.id.textDescription);
             description.setText(workout.getDescription());
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putLong("workoutId", workoutId);
     }
 
     public void setWorkout(long id) {

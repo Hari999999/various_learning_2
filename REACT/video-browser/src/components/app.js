29ff1b36a32f7fc6223432683f400a39ae2fde0b
Joyce Echessa <jokhessa@yahoo.com>
2018-07-24 12:10:55 +0300

    Fetch YouTube search results

---
+++ REACT/video-browser/src/components/app.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './search_bar';
+import VideoList from './video_list';
+
+const API_KEY = 'AIzaSyBPRQw8S0VcVhcjl9B5kg5qbyfGL0bx26Q';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
--- REACT/video-browser/src/index.js
+++ REACT/video-browser/src/index.js
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
-const API_KEY = 'AIzaSyBPRQw8S0VcVhcjl9B5kg5qbyfGL0bx26Q';
-
 ReactDOM.render(<App />, document.querySelector('.container'));

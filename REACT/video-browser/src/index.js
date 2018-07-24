103eb499dbfbd4ded75783947588eb0a4aee3bc4
Joyce Echessa <jokhessa@yahoo.com>
2018-07-24 08:54:25 +0300

    Install youtube-api-search package

--- REACT/video-browser/src/index.js
+++ REACT/video-browser/src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
+const API_KEY = 'AIzaSyBPRQw8S0VcVhcjl9B5kg5qbyfGL0bx26Q';
+
 ReactDOM.render(<App />, document.querySelector('.container'));

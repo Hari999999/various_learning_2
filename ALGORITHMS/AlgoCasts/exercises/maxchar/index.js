55f23166a264cf7d52de53458c7ce0b3bc099fb1
Joyce Echessa <jokhessa@yahoo.com>
2018-09-11 14:04:04 +0300

    Solve maxchar

--- ALGORITHMS/AlgoCasts/exercises/maxchar/index.js
+++ ALGORITHMS/AlgoCasts/exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const arr = str.split('');
+  let obj = {};
+  let maxKey = '';
+  let maxValue = 0;
+
+  for (val of arr) {
+    obj[val] = obj[val] ? obj[val] + 1 : 1;
+  }
+
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (maxValue < obj[key]) {
+        maxKey = key;
+        maxValue = obj[key]
+      }
+    }
+  }
+
+  return maxKey;
+}
 
 module.exports = maxChar;
